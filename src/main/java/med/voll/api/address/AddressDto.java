fed9e24f22f48759589ac008b5ddbb33d74bac8a
Erick Michel <michel172002@gmail.com>
2023-07-11 21:04:18 -0300

    created DoctorDto, AddressDto and Enum Specialty

---
+++ src/main/java/med/voll/api/address/AddressDto.java
@@ -0,0 +1,4 @@
+package med.voll.api.address;
+
+public record AddressDto(String street, String district, String cep, String city, String uf, String number, String complement) {
+}
---
+++ src/main/java/med/voll/api/doctor/DoctorDto.java
@@ -0,0 +1,7 @@
+package med.voll.api.doctor;
+
+import med.voll.api.address.AddressDto;
+
+public record DoctorDto(String name, String email, String crm, Specialty specialty, AddressDto addressDto) {
+
+}
