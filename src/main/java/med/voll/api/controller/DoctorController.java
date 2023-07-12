c9c38c9be05a9cc0f178ed97f7d3d9e0dc09dcc7
Erick Michel <michel172002@gmail.com>
2023-07-11 21:52:34 -0300

    created interface doctorRepository

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
+import med.voll.api.doctor.Doctor;
 import med.voll.api.doctor.DoctorDto;
+import med.voll.api.doctor.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("doctors")
 public class DoctorController {
 
+    @Autowired
+    private DoctorRepository doctorRepository;
+
     @PostMapping
     public void register(@RequestBody DoctorDto doctorDto){
-        System.out.println(doctorDto);
+        doctorRepository.save(new Doctor(doctorDto));
     }
 }
